95a47c214269bbea7f84e9dc0e0836f9465981f8
Luciany Laurentino <lucylaurentino@gmail.com>
2017-10-23 21:52:18 +1300

    finished program

--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace testCsharp
 {
     class Program
     {
+        static List<string> cars = new List<string> { "Audi", "Mazda", "Ford", "Toyota" };
+        static List<string> drivers = new List<string> { "BOB", "Lisa", "Tim", "Simon" };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(DisplayDriver());
+            Console.WriteLine(DisplayDriver());
+            Console.WriteLine(DisplayDriver());
+            Console.WriteLine(DisplayDriver());
         }
+        static int GenerateRandomNumber(List<string> myList)
+        {
+            var rand = new Random();
+            return rand.Next(1, myList.Count);
+        }
+        static string DisplayDriver()
+        {
+            var selectedCars = cars[GenerateRandomNumber(cars)];
+            var selectedDriver = drivers[GenerateRandomNumber(drivers)];
+            return $"selected Driver = {selectedDriver}\nSelected Car = {selectedCars}";
+        }
+
     }
 }
